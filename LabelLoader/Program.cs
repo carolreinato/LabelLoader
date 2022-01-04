fe9361d6c85a6c5dd99eba4adef997aebced5c64
Caroline Reinato <cr.reinato@gmail.com>
2022-01-03 21:27:11 -0300

    Initial commit 👌 merece 10

--- LabelLoader/Program.cs
+++ LabelLoader/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using LabelLoader.Buservice;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json.Serialization;
 
 namespace LabelLoader
 {
@@ -6,7 +13,19 @@ namespace LabelLoader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+
+            var list = new List<Message>();
+            //{ "ItemName": "meat",
+            // "Ingredients": ["diary","gluten","soy"] }
+
+            var labelLoader = new LabelLoaderService(list, configuration["serviceBus:connectionString"]);
+            labelLoader.SendMessagesAsync(JsonConvert.SerializeObject(new { ItemName = "meat", Ingredients = new List<string>() { "diary", "gluten", "soy" } }));
+            labelLoader.SendMessagesAsync(JsonConvert.SerializeObject(new { ItemName = "ketchup", Ingredients = new List<string>() { "tomato", "sugar", "preservatives" } }));
+
         }
     }
 }
